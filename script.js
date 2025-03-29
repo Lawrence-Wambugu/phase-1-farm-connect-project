c97552901bc9012152fecc6e16705478ac23f4fb
Lawrence-Wambugu <wambugulawrence@gmail.com>
2025-03-29 12:24:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
